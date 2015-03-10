69ad872196d1a4e2c33386e4a9f37f0a2b2ce0b6
Emiliano Jankowski <ejankowski@gmail.com>
2015-03-10 13:47:14 -0300

    Update api-config.js

--- src/common/api-config/api-config.js
+++ src/common/api-config/api-config.js
@@ -5,25 +5,25 @@
   .module('apiConfig',[])
   .constant('ApiConfig', {
     pivotal:{
-      apiToken:'3e511ae6b1a062939bef4748fe725bd1',
+      apiToken:'xxxxx',
       me:'1486154',
       defaultProject:1168276,
       projects:[{
           id:1168276,
           teams:[{
-            id:'a5133f60-d08d-4cff-a413-20e4645b2636',
+            id:'xxxxx',
             name:'Argentina',
             members:[1486154,1560464,1486150,1486158]
           },
           {
-            id:'64858f95-9be5-4159-9d0b-26c4e40d5a5b',
+            id:'xxxxxxx',
             name:'Indo',
             members:[1487148,1512672,1512674,1512676, 1539124]
           }]
       }]
     },
     github:{
-      accessToken:'5c395d3b298f77c9b1cca7cdc266fdf19ca97b1f'
+      accessToken:'xxxx'
     }
   } );
 })();
